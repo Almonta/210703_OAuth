8e4cb81965d2943583977fca50ef74b61a482398
motohiro takagi <tkg.8.mthr@gmail.com>
2021-07-03 18:10:05 +0900

    ルーティングを設定した

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get 'oauth_test/index'
+  # get 'oauth_test/index'
+  root to: 'oauth_test#index'
   # devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users, controllers: {
-    registrations: "users/registrations"
+    registrations: "users/registrations",
+    omniauth_callbacks: "users/omniauth_callbacks"
   }
 end
